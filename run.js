111a8d4b61b6d25a7d7724b03d47c60e9b3acb08
adiixyz <82200440+Adiixyz@users.noreply.github.com>
2021-12-03 20:45:27 +0800

    Update run.js

--- run.js
+++ run.js
@@ -23,10 +23,10 @@ const starts = async (conn = new WAConnection()) => {
 
     fs.existsSync('./session.json') && conn.loadAuthInfo('./session.json')
     conn.on('connecting', () => {
-        start('2', 'menghubungkan...')
+        start('2', 'Connecting bot...')
     })
     conn.on('open', () => {
-        success('2', 'tersambung kak')
+        success('2', 'Connected...')
     })
     await conn.connect({timeoutMs: 30*1000})
         fs.writeFileSync('./session.json', JSON.stringify(conn.base64EncodedAuthInfo(), null, '\t'))
@@ -34,6 +34,37 @@ const starts = async (conn = new WAConnection()) => {
     conn.on('chat-update', async (message) => {
         require('./index.js')(conn, message)
     })
+    
+    conn.on('group-update', async (anu) => {
+		const metdata = await conn.groupMetadata(anu.jid)
+    	const fkontakk = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(anu.jid ? { remoteJid: '6283136505591-1604595598@g.us' } : {})}, message: { "contactMessage":{"displayName": `${metdata.subject}`,"vcard":`BEGIN:VCARD\nVERSION:3.0\nN:2;Adii;;;\nFN:Adii\nitem1.TEL;waid=60199782326:60199782326\nitem1.X-ABLabel:Mobile\nEND:VCARD` }}}
+    if(anu.announce == 'false'){
+    teks = `- [ Group Opened ] -\n\n_Group telah dibuka oleh admin_\n_Sekarang semua member bisa mengirim pesan_`
+    conn.sendMessage(metdata.id, teks, MessageType.text, {quoted: fkontakk})
+    console.log(color('[GROUP]'), color(`Group Opened In ${metdata.subject}`, 'cyan'))
+  }
+  else if(anu.announce == 'true'){
+    teks = `- [ Group Closed ] -\n\n_Group telah ditutup oleh admin_\n_Sekarang hanya admin yang dapat mengirim pesan_`
+    conn.sendMessage(metdata.id, teks, MessageType.text, {quoted: fkontakk})
+    console.log(color('[GROUP]'), color(`Group Closed In ${metdata.subject}`,  'cyan'))
+  }
+  /*else if(!anu.desc == ''){
+    tag = anu.descOwner.split('@')[0] + '@s.whatsapp.net'
+    teks = `- [ Group Description Change ] -\n\nDeskripsi Group telah diubah oleh Admin @${anu.descOwner.split('@')[0]}\n• Deskripsi Baru : ${anu.desc}`
+    conn.sendMessage(metdata.id, teks, MessageType.text, {contextInfo: {"mentionedJid": [tag]}, quoted: fkontakk})
+    console.log(color('[GROUP]'), color(`Group Description Change In ${metdata.subject}`, 'cyan'))
+  }/*
+  else if(anu.restrict == 'false'){
+    teks = `- [ Group Setting Change ] -\n\nEdit Group info telah dibuka untuk member\nSekarang semua member dapat mengedit info Group Ini`
+    conn.sendMessage(metdata.id, teks, MessageType.text, {quoted: fkontakk})
+    console.log(color('[GROUP]'), color(`Group Setting Change In ${metdata.subject}`, 'cyan'))
+  }
+  else if(anu.restrict == 'true'){
+    teks = `- [ Group Setting Change ] -\n\nEdit Group info telah ditutup untuk member\nSekarang hanya admin group yang dapat mengedit info Group Ini`
+    conn.sendMessage(metdata.id, teks, MessageType.text, {quoted: fkontakk})
+    console.log(color('[GROUP]'), color(`Group Setting Change In ${metdata.subject}`,  'cyan'))
+  }
+  }
 }
 
 /************************************
